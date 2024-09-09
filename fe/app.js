33f47bbd1b84e53601dcd69ce6207bc66b031362
Nidhi Singh <nidhis2332@gmail.com>
2024-09-09 16:29:51 +0530

    added program to print odd numbers.

--- fe/app.js
+++ fe/app.js
@@ -6,3 +6,9 @@ console.log('Hello JavaScript');
 function add(var1, var2){ // Defining the function
     return var1 + var2;
 }
+
+// Added for loop
+// print all odd numbers between 1 to 100
+for(let i = 1; i < 100; i+=2){
+    console.log(i);
+}
