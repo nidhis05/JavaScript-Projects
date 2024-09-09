db13e60191437e767d3230fc3e08dde5d938b22d
Nidhi Singh <nidhis2332@gmail.com>
2024-09-09 16:25:56 +0530

    initial commit

---
+++ fe/app.js
@@ -0,0 +1,8 @@
+let a = 5;
+var b = 6;
+console.log(add(a, b)); // Calling the function
+console.log('Hello JavaScript');
+
+function add(var1, var2){ // Defining the function
+    return var1 + var2;
+}
